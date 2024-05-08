444adfd9c4f8f0b771df5c98acd94cff9b9a82cd
Nitis <nitis.182546@gmail.com>
2024-05-08 23:12:00 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$title = "Home";
+require_once('./layout/header.php');
+?>
+<body>
+    <?php require_once('./layout/nav.php'); ?>
+    <div class="container mt-3">
+        <h1 class="text-center">ระบบ เพิ่ม ลบ แก้ไข ข้อมูล</h1>
+    </div>
+</body>
+</html>
